f221acc364833636e5c99fddc5b86c434d7a35ba
derkauzigekoala <derkauzigekoala@gmail.com>
2023-01-26 18:27:18 +0100

    Add square method

--- src/main/java/fhtw/squareapi/SquareApiController.java
+++ src/main/java/fhtw/squareapi/SquareApiController.java
@@ -1,7 +1,13 @@
 package fhtw.squareapi;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SquareApiController {
+
+    @GetMapping("api/square")
+    public int square(int n) {
+        return n * n;
+    }
 }
